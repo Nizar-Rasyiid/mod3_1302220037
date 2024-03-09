5bd379b9b00bedf64c7db8cfd076962776d7eb2a
Nizar-Rasyiid <nizarrasyiid1221@gmail.com>
2024-03-09 07:53:19 +0700

    first commit

---
+++ KPL_MOD_3_1302220037_AFH/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KPL_MOD_3_1302220037_AFH
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
